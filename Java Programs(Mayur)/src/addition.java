a713d8dbd9edc5463c52f85efb244c525e115852
Admin <Admin@192.168.1.102>
2018-09-18 15:23:33 +0530

    These are few file handling programs

---
+++ Java Programs(Mayur)/src/addition.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a,b,sum=0;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter First Number :\t");
+	    a=s.nextInt();
+	    System.out.println(a);
+
+	}
+
+}
---
+++ Java Programs(Mayur)/src/ioprograms/FileHand_fileread.java
@@ -0,0 +1,18 @@
+package ioprograms;
+import java.io.*;
+public class FileHand_fileread {
+
+	public static void main(String[] args) 
+	{
+		try
+		{
+			FileInputStream f=new FileInputStream("abc.txt");
+			String i=f.read();
+		
+		
+		
+		
+		}
+	}
+
+}
